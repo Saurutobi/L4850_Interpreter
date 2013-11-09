b7bcb5206f445d8c24fb40bdfc3ac06e8d5b76df
Saurutobi <saurutobi@gmail.com>
2013-11-09 16:09:19 -0500

    modified for single file input

--- Project_2_Eval/src/interpreter/L485Interpreter.java
+++ Project_2_Eval/src/interpreter/L485Interpreter.java
@@ -20,31 +20,10 @@ public class L485Interpreter
 	
 	private static void repl()
 	{
-		//This section is modified to ask for 2 types of input: a single line input, or a directory path for batch processing
-		System.out.println("1 for single input from command\n2 for single file input from command\n3 for directory input");
-		Scanner operation = new Scanner(System.in);
-		System.out.print( "====> ");
-		String op = operation.nextLine();
+		//Modified for grader's batch input
 		try 
 		{
-			System.out.print( "====> ");
-			//single input from command
-			if(op.equals("1"))
-			{
-				String [] temp = new String[1];
-				temp[0] = operation.nextLine();
-				processCode(temp);
-			}
-			//Single file input from command
-			if(op.equals("2"))
-			{
-				processCode(getSingleFileInput());
-			}
-			//directory input
-			if(op.equals("3"))
-			{
-				processCode(getDirectoryInput());
-			}
+			processCode(getSingleFileInput());
 		}
 		catch (IOException e)
 		{
@@ -76,35 +55,6 @@ public class L485Interpreter
 			while((curLine = buff.readLine()) != null)
 			{
 				retVal.add(curLine);
-			}
-		}
-		catch(Exception e)
-		{
-			System.out.println("It didn't work, try again");
-		}
-		return retVal.toArray(new String[retVal.size()]);
-	}
-		
-	//This method now returns an array for quicker testing
-	private static String [] getDirectoryInput() throws IOException
-	{
-		List<String> retVal = new ArrayList<String>();
-		System.out.println("Enter the full file path of your directory of input");
-		System.out.print( "====> ");
-		try
-		{
-			Scanner scan = new Scanner(System.in);
-			String dirName = scan.nextLine();
-			File directory = new File(dirName);
-			File [] contents = directory.listFiles();
-			for(File currentFile : contents)
-			{
-				BufferedReader buff = new BufferedReader(new FileReader(currentFile.getAbsolutePath()));
-				String curLine;
-				while((curLine = buff.readLine()) != null)
-				{
-					retVal.add(curLine);
-				}
 			}
 		}
 		catch(Exception e)
