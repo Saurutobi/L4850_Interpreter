f4e2aea794ce5ca71c2d57944e618427578b62c7
Jason Pearson <jjpearson8611@gmail.com>
2013-12-04 16:00:24 -0500

    Closures coming on deck!!

---
+++ Project_3_Func/src/util/Closure.java
@@ -0,0 +1,24 @@
+package util;
+
+import java.util.ArrayList;
+
+public class Closure extends Function{
+
+	public Closure(){
+		
+		
+		
+	}
+
+	@Override
+	public Value invoke(Environment env, ArrayList<Value> args) throws L485Error {
+		//something needs to go here
+		return null;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
